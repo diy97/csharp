71133b350b7357572df0ffb5beda44c124aebbbd
diy97 <44125982+diy97@users.noreply.github.com>
2019-05-12 21:58:53 +0800

    Add files via upload

--- Array_List_Hashtable/Program.cs
+++ Array_List_Hashtable/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,51 @@ namespace Array_List_Hashtable
     {
         static void Main(string[] args)
         {
+
+            /*
+             *ArrayList是通过索引的方式赋值和取值（数组）,和数组一致长度固定
+             */
+            ArrayList arrayList = new ArrayList();
+            Student stu1 = new Student("10001","张三","201901","普通班");
+            Student stu2 = new Student();
+            stu2.StuNum = "10002";
+            stu2.Name = "李四";
+            stu2.ClassId = "201902";
+            stu2.ClassName = "精英班";
+            Student stu3 = new Student("10003", "王五", "201903", "普通班");
+
+            //arrayList.Add(stu1);
+            //arrayList.Add(stu2);
+            //arrayList.Add(stu3);
+
+            //for (int i = 0; i < arrayList.Count; i++)
+            //{
+            //    Student stu = (Student)arrayList[i];
+            //    Console.WriteLine(stu.StuNum+"-"+stu.Name+"-"+stu.ClassId+"-"+stu.ClassName);
+            //}
+
+
+
+            /*
+             *  Hashtalbe 是键值形式，键key  值value
+             */
+            Hashtable ht = new Hashtable();
+            ht.Add("ZS", stu1);
+            ht.Add("LS", stu2);
+            ht.Add("WW", stu3);
+
+            //Student stu = (Student)ht["ZS"];
+
+            //Console.WriteLine(stu.StuNum + "-" + stu.Name + "-" + stu.ClassId + "-" + stu.ClassName);
+
+            foreach (Student item in ht.Keys)
+            {
+                Console.WriteLine(item.StuNum + "-" + item.Name + "-" + item.ClassId + "-" + item.ClassName);
+
+            }
+
+            Console.ReadKey();
+
         }
     }
 }
---
+++ Array_List_Hashtable/Student.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_List_Hashtable
+{
+    /// <summary>
+    /// 学生信息
+    /// </summary>
+    class Student
+    {
+        //成员变量
+        private string stuNum;
+        private string name;
+        private string classId;
+        private string className;
+
+
+        //以下是属性
+        /// <summary>
+        /// 学号
+        /// </summary>
+        public string StuNum
+        {
+            get { return stuNum;}
+            set{ stuNum = value; }
+        }
+
+        /// <summary>
+        /// 姓名
+        /// </summary>
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        /// <summary>
+        /// 班级ID
+        /// </summary>
+        public string ClassId
+        {
+            get { return classId; }
+            set { classId = value; }
+        }
+
+        /// <summary>
+        /// 班级名称
+        /// </summary>
+        public string ClassName
+        {
+            get { return className; }
+            set { className = value; }
+        }
+
+        //无参构造函数
+        public Student() { }
+
+        //有参构造函数
+        /// <summary>
+        /// 学生信息
+        /// </summary>
+        /// <param name="stuNum">学号</param>
+        /// <param name="name">姓名</param>
+        /// <param name="classId">班级编号</param>
+        /// <param name="className">班级名称</param>
+        public Student(string stuNum,string name,string classId,string className)
+        {
+            this.stuNum = stuNum;
+            this.name = name;
+            this.classId = classId;
+            this.className = className;
+        }
+
+    }
+
+}
+
